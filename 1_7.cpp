db4c304982351cdca91cd977dbbb39135a87fe62
Pp7117 <163641960+Pp7117@users.noreply.github.com>
2024-03-29 17:45:24 +0530

    Add files via upload

---
+++ 1_7.cpp
@@ -0,0 +1,24 @@
+  #include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10]={2,1,3,4,2,3,5,6,3,1};
+    int* pos=arr;
+    for(int i=0;i<10;i++)    
+    {
+        for(int j=pos[i+1];j<=10;j++)
+        {
+            if(pos[i]=
+            000=pos[j])
+            {
+                cout<<pos[i];
+            }
+            else
+            ;
+        }
+
+    }                                                      00 0                                          
+}
+
+
